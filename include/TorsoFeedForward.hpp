d4e592f9b1d83a9f31eda06ef61387490c04769e
TonPeters <peterstmc@gmail.com>
2015-05-14 10:12:43 +0200

    added feed forward compensation legs

--- include/TorsoFeedForward.hpp
+++ include/TorsoFeedForward.hpp
@@ -33,13 +33,14 @@ namespace SERGIOCUSTOM
     // Declaring global variables
     doubles angles;
     doubles forces;
-    double angleForce;
+    doubles angleForce;
 
     // Declaring global variables
     double X1; // BG(q0)
     double X1_sq; // BG(q0)^2
     double X2; // an(A,G,B)
     double X3; // an(B,G,D)
+    double dq1_dq0; // Partial derivative dq1/dq0
 
     // Declaring global constants
     double C1; // AE^2+AC^2
@@ -57,6 +58,30 @@ namespace SERGIOCUSTOM
     double C13; // -2*GD
     double C14; // an(J,G,D)
 
+    double C24; // HJ^2-JK^2
+    double C25; // -2*JK
+    double C26; // AC^2-AE^2
+    double C27; // -2*AE
+    double C28; // JK*r_sp*r_gear1
+    double C29; // AE*r_sp*r_gear2
+
+    double g;   // gravity constant
+    double gravity_offset;
+    double l1; // length link 1
+    double l2; // length link 2
+    double l3; // length link 3
+
+    double P1; // estimate (m1*lcm1+m2*l1+m3*l1)
+    double P2; // estimate (m2*lcm2+m3*l2)
+    double P3; // estimate (m3*lcm3)
+    double Kmu1; // estimate proportional friction leg
+    double Kc1; // estimate coulomb friction leg
+    double Kc2; // estimate coulomb friction trunk
+    double Kd2; // estimate direction independent friction trunk
+
+    doubles params_q0q1; // array containing parameters of the polynomial estimate
+
+
 
     // variables set by properties
     double m_arms;
@@ -69,6 +94,10 @@ namespace SERGIOCUSTOM
     bool configureHook();
     bool startHook();
     void updateHook();
+
+    private:
+
+    double change_q1_q0( double q0 );
     };
 }
 #endif
--- src/TorsoFeedForward.cpp
+++ src/TorsoFeedForward.cpp
@@ -29,6 +29,7 @@ bool TorsoFeedForward::configureHook()
     X1_sq = 0.0;
     X2 = 0.0;
     X3 = 0.0;
+    double tmp = 0.0;
 
     // initialize constants
     C1 = 0.150052;
@@ -45,6 +46,35 @@ bool TorsoFeedForward::configureHook()
     C12 = 0.132722;
     C13 = -0.155586;
     C14 = 2.913677;
+    C24 = 0.162258;
+    C25 = -0.157983;
+    C26 = -0.096279;
+    C27 = -0.701898;
+	C28 = 1075.352975;
+	C29 = 2756.347553;
+    g    = 9.81;
+    gravity_offset = 0.0349;
+    l1 = 0.389978;
+    l2 = 0.411498;
+    l3 = 0.469972;
+
+    // system estimates
+//    P1 = 2.7955;
+    P1 = 6.58;
+    P2 = 4.32;
+    P3 = 2.47;
+    Kmu1 = 0.288;
+    Kc1 = 0.0075;
+    Kc2 = 0.029;
+    Kd2 = 0.0097;
+
+    // polynomial values
+    params_q0q1.assign(5,0.0);
+    params_q0q1[4] = 11.68;
+    params_q0q1[3] = -24.33;
+    params_q0q1[2] = 20.18;
+    params_q0q1[1] = -7.847;
+    params_q0q1[0] = 2.957;
 
     return true;
 }
@@ -55,8 +85,9 @@ bool TorsoFeedForward::startHook()
     input.assign(2,0.0);
     input_vel.assign(2,0.0);
     output.assign(2,0.0);
-    angles.assign(2,0.0);
+    angles.assign(3,0.0);
     forces.assign(2,0.0);
+    angleForce.assign(2,0.0);
 
     // Check validity of Ports:
     if ( !outport.connected() ) {
@@ -81,30 +112,72 @@ void TorsoFeedForward::updateHook()
         X3 = acos((C12-X1_sq)/(C13*X1));
         angles[1] = C14-X2-X3;
         angles[2] = acos((input[1]*input[1]-C4)/C5)+C6;
-
+		
+        //log(Error)<< "Angle 0 = "<<angles[0]<< ", Angle 1 = "<<angles[1]<<'Angle 2 '<< angles[2]<<endlog();
         // angle at which the force is applied to the trunk
-        angleForce = acos((-0.1685+0.0062+input[1]*input[1])/(2*0.079*input[1]));
+        angleForce[1] = acos((C24-input[1]*input[1])/(C25*input[1]));
+        angleForce[0] = acos((C26-input[0]*input[0])/(C27*input[0]));
+        //log(Error)<< "FAngle 1 = "<<angleForce[0]<< ", Fangle 2 = "<<angleForce[1]<<endlog();
+
+        // current gear ratio between angle0 and angle1;
+        dq1_dq0 = change_q1_q0(angles[0]);
+        //log(Error)<<" dq1/dq0 "<<dq1_dq0<<endlog();
 
         // Calculate gravity compensation force needed
-        forces[1] = (2.7955+0.47*m_arms)*124.19*cos(angles[2]+angles[0]-angles[1])/sin(angleForce);
+//        forces[1] = (P3+0.47*m_arms)*124.19*cos(angles[2]+angles[0]-angles[1])/sin(angleForce);
+        forces[1] = g*(P3+l3*m_arms)*cos(angles[2]+angles[0]-angles[1]+gravity_offset)/(sin(angleForce[1])*C28);
+        forces[0] = g*(cos(angles[0])*(P1+l1*m_arms)+(dq1_dq0-1)*cos(angles[1]-angles[0])*(P2+l2*m_arms)+(1-dq1_dq0)*cos(angles[2]-angles[1]+angles[0]+gravity_offset)*(P3+l3*m_arms))/(sin(angleForce[0])*C29);
 
-        // Add friction force needed
-        forces[1] = forces[1]+91.9194;            // direction independant friction term
+        //log(Error)<< "tau G 1 = "<<forces[0]<< ", tau G 2 = "<<forces[1]<<endlog();
+        // TODO. add spring forces
+
+		
+        // Add friction forces
+//        forces[1] = forces[1]+91.9194;            // direction independant friction term
+        forces[1] = forces[1]+Kd2;            // direction independent friction term
         inport_vel.read(input_vel);
-        if (input_vel[1]>0){                    // direction dependant friction term
-            forces[1] = forces[1]+394.8329;
+        if (input_vel[1]>0){                    // Coulomb friction term
+            forces[1] = forces[1]+Kc2;
         } else if (input_vel[1]<0){
-            forces[1] = forces[1]-394.8329;
+            forces[1] = forces[1]-Kc2;
+        }
+        if (input_vel[0]>0){
+            if (forces[0]>0){
+                forces[0] = forces[0]*(1+Kmu1);
+            } else {
+                forces[0] = forces[0]*(1-Kmu1);
+            }
+            forces[0] += Kc1;
+        } else if (input_vel[0] <0 ) {
+            if (forces[0]>0){
+                forces[0] = forces[0]*(1-Kmu1);
+            } else {
+                forces[0] = forces[0]*(1+Kmu1);
+            }
+            forces[0] -= Kc1;
         }
 
-        // convert to output voltage
-        output[1] = forces[1]*0.00025156;
-        output[0] = 0.0;
+        // convert to output motor torque
+//        output[1] = forces[1]*0.00025156;
+        output[1] = forces[1];
+        output[0] = forces[0];
+        log(Error)<< "Torque 1 = "<<output[0]<< ", Torque 2 = "<<output[1]<<endlog();
 
     }
 
     // write output to port
     outport.write(output);
+}
+
+double TorsoFeedForward::change_q1_q0(double q0)
+{
+    double out = params_q0q1[0];
+    double q0_pow = q0;
+    for (int i=1; i<params_q0q1.size(); i++){
+         out += params_q0q1[i]*q0_pow;
+         q0_pow = q0_pow*q0;
+    }
+    return out;
 }
 
 ORO_CREATE_COMPONENT(SERGIOCUSTOM::TorsoFeedForward)
